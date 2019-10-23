5aa828bc2b05156f6250c723d8c0775ff188dd71
nekonbu72 <nakatomo222@gmail.com>
2019-10-23 21:51:07 +0900

    　

--- src/components/ViewSplitter.vue
+++ src/components/ViewSplitter.vue
@@ -7,7 +7,6 @@
       class="gutter"
       draggable="true"
       @dragstart="handleDragStart"
-      @drag="handleDrag"
       @dragend="handleDragEnd"
       v-show="isSolidShown"
     ></div>
@@ -27,6 +26,7 @@ export default {
        */
       solid: {},
       beforeClientX: -1,
+      afterClientX: -1,
       sample: "text ".repeat(50),
       isSolidShown: true
     };
@@ -51,47 +51,30 @@ export default {
       });
     },
     /**
-     * @param {DragEvent} e
+     * @param {DragEvent} event
      */
-    handleDragStart(e) {
-      console.log("dragstart");
-      e.dataTransfer.setData("text/plain", "");
-
-      console.log("before:", e.clientX);
-      this.beforeClientX = e.clientX;
+    handleDragStart(event) {
+      // Firefox52 では setData しないと drag イベントが発生しない
+      event.dataTransfer.setData("text/plain", "");
+      this.beforeClientX = event.clientX;
     },
 
-    /**
-     * @param {DragEvent} e
-     */
-    handleDrag(e) {
-      console.log("dragging", e.clientX);
-    },
-
-    /**
-     * @param {DragEvent} e
-     */
-    handleDragEnd(e) {
-      console.log("before:", this.beforeClientX, " after:", e.clientX);
-
-      const move = e.clientX - this.beforeClientX;
-
+    handleDragEnd() {
+      const moveX = this.afterClientX - this.beforeClientX;
       const style = window.getComputedStyle(this.solid);
-
       const widthPx = style.getPropertyValue("width");
       const width = parseFloat(widthPx);
-
-      console.log("mv:", move, " st:", style, " px:", widthPx, " w:", width);
-
-      this.solid.style.width = `${width + move}px`;
-      this.beforeClientX = e.clientX;
+      this.solid.style.width = `${width + moveX}px`;
+      this.beforeClientX = this.afterClientX;
     },
     /**
-     * @param {DragEvent} e
+     * @param {DragEvent} event
      */
-    handleDragOver(e) {
-      e.preventDefault();
-      e.dataTransfer.dropEffect = "move";
+    handleDragOver(event) {
+      event.preventDefault();
+      event.dataTransfer.dropEffect = "move";
+      // dragend イベントでは clientX を取得できないため dragover で取得
+      this.afterClientX = event.clientX;
     }
   }
 };
@@ -102,10 +85,7 @@ export default {
 .wrapper {
   display: flex;
   height: 95vh;
-  /* width: 200vh; */
   border: solid 0.5vh green;
-  /* resize: both;
-  overflow: scroll; */
 }
 
 .item {
